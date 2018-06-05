93b535927bee2dcb01c85f0bc433c0fef840dbd2
Henri Pirinen <henripirinen@hotmail.fi>
2018-06-05 14:42:18 +0300

    analyse mqtt and insert data to postgresql

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
-var express = require('express');
-var socket = require('socket.io');
-var mqtt = require('mqtt');
+const keys = require('./keys');
+const express = require('express');
+const socket = require('socket.io');
+const mqtt = require('mqtt');
+const pg = require('pg');
+const uuid = require('uuid/v1');
 
-var clientMQTT = mqtt.connect('mqtt://192.168.2.45'); //MQTT server address
+const pool = new pg.Pool({
+	user: keys.db.dbUser,
+	host: keys.db.dbHost,
+	database: keys.db.dbName,
+	password: keys.db.dbUserPassword,
+	port: keys.db.dbPort
+});
 
-clientMQTT.on('connect', function () {
+var clientMQTT = mqtt.connect(keys.address.mqtt);
+
+clientMQTT.on('connect', () => {
 	clientMQTT.subscribe('vehicleData');
 	clientMQTT.subscribe('vehicleExternalCommand');
 });
 
+clientMQTT.on('message', (topic, message) => {
+	if (topic !== 'vehicleExternalCommand') {
+		let telemetry = JSON.parse(message.toString());
+		let cellID = 0;
+		let insertQuery = '';
 
-clientMQTT.on('message', function (topic, message) {
-	console.log(message.toString())
-	//clientMQTT.end()
+		for (let x = 0; x < telemetry.group.length; x++) { //For telemetry data structure go to vehicle-server index.js. Object @ line 10.
+			for (let i = 0; i < telemetry.group[i].voltage.length; i++ , cellID++) { //Voltage & temperature array length is equal
+				let measurementUUID = uuid();
+				let result = analyse(telemetry.group[i].voltage[i], telemetry.group[i].temperature[i]);
+
+				insertQuery += "INSERT INTO measurement (uuid, clock, cell_id) \
+								VALUES ('" + measurementUUID + "', NOW(), " + cellID + "); \
+								INSERT INTO voltage (uuid, measured_voltage, is_voltage_ok, measurement_id) \
+								VALUES ('" + uuid() + "', " + telemetry.group[i].voltage[i] + ", " + (result.voltage === null ? true : false) + ", '" + measurementUUID + "'); \
+								INSERT INTO temperature (uuid, measured_temp, is_temp_ok, measurement_id) \
+								VALUES ('" + uuid() + "', " + telemetry.group[i].temperature[i] + ", " + (result.temperature === null ? true : false) + ", '" + measurementUUID + "');";
+
+				if(result.voltage != null || result.temperature != null){
+					let logMsg;
+					if(result.voltage != null) logMsg += result.voltage;
+					if(result.temperature != null) logMsg += result.temperature;
+					
+					insertQuery += "INSERT INTO log (uuid, error_msg, cleared, measurement_uuid) \
+					VALUES ('" + uuid() + "', '" + logMsg + "', false, '" + measurementUUID + "');";
+				}
+			}
+		}
+
+		pool.query(insertQuery, (err, res) => {
+			console.log(res);
+			console.log(err);
+		});
+	}
 });
 
 
 var app = express();
-var server = app.listen(4000, function () { //Start server
+var server = app.listen(4000, () => { //Start server
 	console.log("Listening port 4000 @ localhost")
 	console.log("MQTT is subscribed to 'vehicleData & vehicleExternalCommand'");
 });
 
 var io = socket(server);
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
 	socket.emit('webSocket', {		//Send notification to new client 
 		message: 'WebSocket connected!',
 		handle: 'Server'
 	});
 
-	socket.on('command', function (data) { //Write command to arduino via USB
+	socket.on('command', (data) => { //Write command to arduino via USB
 
 		switch (data.target) {
 			case "controller_1":
-                //TODO
+				//TODO
 				break;
 			case "controller_2":
-                //TODO
+				//TODO
 				break;
 			case "inverter":
 				//TODO
@@ -52,18 +93,37 @@ io.on('connection', function (socket) {
 	});
 });
 
-function validateJSON(string) { //Validate JSON string
+var validateJSON = (string) => { //Validate JSON string
 	try {
 		JSON.parse(string);
 	} catch (e) {
-		//console.log(e);
+		console.log(e);
 		return false;
 	}
 	return true;
 }
 
 var uploadData = () => {
-	clientMQTT.publish('vehicleData', '{some_data}');
+	clientMQTT.publish('vehicleExternalCommand', '$commandFromRemote');
+	//TODO: commands -> L64
+}
+
+var analyse = (voltage, temperature) => {
+	let errorMsg = {'voltage':null,'temperature':null};
+
+	if(voltage > 3.80 || voltage < 2.75){
+		if(voltage > 3.80) errorMsg.voltage = 'WARNING: HIGH VOLTAGE ( ' + voltage + 'V ). ';
+		if(voltage < 2.75) errorMsg.voltage = 'WARNING: LOW VOLTAGE ( ' + voltage + 'V ). ';
+		//console.log('Found something!');
+	}
+
+	if(temperature > 90 || temperature < 0){
+		if(temperature > 90) errorMsg.temperature = 'WARNING: HIGH TEMPERATURE ( ' + temperature + 'C ). ';
+		if(temperature < 0) errorMsg.temperature = 'WARNING: LOW TEMPERATURE ( ' + temperature + 'C ). ';
+		//console.log('Found something!');
+	}
+
+	return errorMsg;
 }
 
 setInterval(uploadData, 5000);
