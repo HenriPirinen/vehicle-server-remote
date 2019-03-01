c7486d0b01bfeac57048f7b7ceea14e472e5d24b
HenriPirinen <henripirinen@hotmail.fi>
2019-03-01 17:07:17 +0000

    Removed access to dot files

--- index.js
+++ index.js
@@ -109,7 +109,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
